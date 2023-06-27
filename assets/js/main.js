4a75d9efa8a364994cbe6352d1c1e8616614e6e0
Sati <s.urkumbayeva@mail.ru>
2023-06-27 11:23:37 +0600

    Responsive layout of an online store on Bootstrap 5. Part 7

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,33 @@ window.addEventListener('scroll', function () {
 
     document.getElementById('header-nav').classList.toggle('headernav-scroll', window.scrollY > 135);
 });
+
+$(document).ready(function() {
+
+    $(".owl-carousel-full").owlCarousel({
+
+        margin: 20,
+        responsive: {
+
+            0: {
+
+                items: 1
+            },
+
+            576: {
+
+                items: 2
+            },
+
+            768: {
+
+                items: 3
+            },
+
+            992: {
+
+                items: 4
+            }
+        }
+    });
+});
